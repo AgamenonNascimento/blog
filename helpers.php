db09b0b0cf18c4dca75524df01410f133e3ace03
Agamenon <agamenonnas@gmail.com>
2023-08-14 09:46:54 -0300

    implemtando function

--- helpers.php
+++ helpers.php
@@ -3,3 +3,13 @@ function saudacao()
 {
   return ('Bom dia');
 }
+
+function formatarValor(float $valor = null): string
+{
+  return number_format(($valor ? $valor : 0), 2, ',', '.');
+}
+
+function formatarNumero(int $numero = null): string
+{
+  return number_format(($numero ? $numero : 0), 0, ',', '.');
+}
